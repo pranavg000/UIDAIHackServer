9224e87475fcf5baf54c904a81596605a91ffa5d
Pranav <pranavgupta000@gmail.com>
2021-10-29 17:01:05 +0530

    added geocode, format changes and added state checks

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('rejectrequest', views.rejectRequest, name='rejectrequest'),
     path('getpublickey', views.getPublicKey, name='getpublickey'),
     # path('oekyc', views.oekyc, name='oekyc'),
-    # path('updateaddress', views.updateAddress, name='updateAddress'),
+    path('updateaddress', views.updateAddress, name='updateAddress'),
     # path('sendmessage', views.sendMessage, 'sendMessage'),
     path('postekyc', views.POSTekyc, name='POSTekyc'),
     path('getekyc', views.GETekyc, name='GETekyc'),
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from os import stat
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from rest_framework.parsers import JSONParser
@@ -8,15 +9,18 @@ from .models import *
 from .decorators import check_token, request_interface
 from django.conf import settings
 from django.core.files.storage import default_storage
-
+import requests
+import json
 from pyfcm import FCMNotification
 from .utils import getRandAlNum, getRandAl
+from math import dist, sin, cos, sqrt, atan2, radians
 
 push_notification_service = FCMNotification(api_key=settings.FIREBASE_SERVER_KEY)
-
+radar_api_key = 'prj_test_sk_f07c23cb9ac9fff33d38ccbb366375953cc0abc1'
 
 AUTH_TOKEN_LEN = 16
 SHAREABLE_CODE_LEN = 9
+ADDRESS_FIELDS = ['co','house','street','lm','lo','vtc','subdist','dist','state','country','pc','po']
 
 def genAuthToken():
     return getRandAlNum(AUTH_TOKEN_LEN)
@@ -29,29 +33,29 @@ def genShareableCode():
 
 def callOTPAPI(uid):
     #TODO: OTP API (AN) 
-    transactionNo = ""
-    while(transactionNo=="" or OTPAPISim.objects.filter(transactionNo=transactionNo).exists()):
-        transactionNo = getRandAlNum(5)
+    transactionID = ""
+    while(transactionID=="" or OTPAPISim.objects.filter(transactionID=transactionID).exists()):
+        transactionID = getRandAlNum(5)
     
     
     if OTPAPISim.objects.filter(uid=uid).exists():
         banda = OTPAPISim.objects.get(uid=uid)
-        banda.transactionNo = transactionNo
+        banda.transactionID = transactionID
         banda.save()
-        return transactionNo
+        return transactionID
     
     uidToken = ""
     while(uidToken=="" or OTPAPISim.objects.filter(uidToken=uidToken).exists()):
         uidToken = getRandAlNum(16)
 
-    OTPAPISim.objects.create(uid=uid, transactionNo=transactionNo, uidToken=uidToken)
+    OTPAPISim.objects.create(uid=uid, transactionID=transactionID, uidToken=uidToken)
 
-    return transactionNo
+    return transactionID
 
-def verifyOTPAuthAPI(transactionNo, otp):
+def verifyOTPAuthAPI(transactionID, otp):
     if otp == '12345':
-        if OTPAPISim.objects.filter(transactionNo=transactionNo).exists():
-            uidToken = OTPAPISim.objects.get(transactionNo=transactionNo).uidToken
+        if OTPAPISim.objects.filter(transactionID=transactionID).exists():
+            uidToken = OTPAPISim.objects.get(transactionID=transactionID).uidToken
             return True, uidToken
     return False, None
 
@@ -63,6 +67,42 @@ def sendPushNotification(deviceID, messageTitle, messageBody, dataMessage=None):
 
     return False        # Pray that this never happens
 
+def getDistance(p1, p2):
+        # approximate radius of earth in km
+        R = 6373.0
+
+        lat1 = radians(p1[0])
+        lon1 = radians(p1[1])
+        lat2 = radians(p2[0])
+        lon2 = radians(p2[1])
+
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        return R * c    # in km
+
+def getCoord(address):
+    addressS = ""
+    IGNORE_FIELDS = ['co', 'po', 'lm']
+    for field in ADDRESS_FIELDS:
+        if field in IGNORE_FIELDS:
+            continue
+        if address.get(field):
+            addressS += ('+'.join(address[field].split())+',+')
+    
+
+    addressS = addressS[:-1]
+
+    response = requests.get('https://api.radar.io/v1/geocode/forward?query='+addressS, headers={'Authorization': radar_api_key})
+    if response.status_code != 200:
+        print("FFFFFFFFFFFFFFFFFF Geocode failed at ",addressS)
+        return False
+    coord = json.loads(response.content)['addresses'][0]
+    return (coord['latitude'], coord['longitude'])
+
 
 @api_view(['POST'])
 @request_interface(['uid'])
@@ -70,22 +110,22 @@ def authUID(request):
     if request.method == 'POST':
         # data = JSONParser.parse(request)
         
-        transactionNo = callOTPAPI(request.data['uid'])
+        transactionID = callOTPAPI(request.data['uid'])
         
-        return JsonResponse({'transactionNo': transactionNo}, status=200)
+        return JsonResponse({'transactionID': transactionID}, status=200)
     return JsonResponse({}, status=400)
 
 
 @api_view(['POST'])
-@request_interface(['transactionNo', 'otp', 'deviceID', 'publicKey'])
+@request_interface(['transactionID', 'otp', 'deviceID', 'publicKey'])
 def authOTP(request):
     if request.method == 'POST':
         # data = JSONParser.parse(request)
-        
-        result, uidToken = verifyOTPAuthAPI(request.data['transactionNo'], request.data['otp'])
+        transactionID = request.data['transactionID']
+        result, uidToken = verifyOTPAuthAPI(transactionID, request.data['otp'])
         
         if not result:
-            return JsonResponse({}, status=403)
+            return JsonResponse({'body': "Wrong OTP", 'transactionID': transactionID}, status=403)
 
         deviceID = request.data['deviceID']
         publicKey = request.data['publicKey']
@@ -96,13 +136,13 @@ def authOTP(request):
             profile.deviceID = deviceID
             profile.save()
 
-            return JsonResponse({'shareableCode': profile.shareableCode, 'authToken': profile.authToken, 'uidToken': uidToken}, status=200)
+            return JsonResponse({'shareableCode': profile.shareableCode, 'authToken': profile.authToken, 'uidToken': uidToken, 'transactionID': transactionID}, status=200)
 
         shareableCode = genShareableCode()
         authToken = genAuthToken()
 
         AnonProfile.objects.create(uidToken=uidToken, authToken=authToken, deviceID=deviceID, publicKey=publicKey, shareableCode=shareableCode)
-        return JsonResponse({'shareableCode': shareableCode, 'authToken': authToken, 'uidToken': uidToken}, status=200)
+        return JsonResponse({'shareableCode': shareableCode, 'authToken': authToken, 'uidToken': uidToken, 'transactionID': transactionID}, status=200)
 
     return JsonResponse({}, status=403)
 
@@ -121,32 +161,34 @@ def sendRequest(request):
 
         Transaction.objects.filter(lender=lender, requester=requester).update(state='aborted')
         transaction = Transaction.objects.create(lender=lender, requester=requester)
-        if sendPushNotification(lender.deviceID, "New Address Request", "New address request has been initiated", {'encryptedMessage': request.data['message'], 'transactionNo': transaction.transactionNo, 'status': transaction.state}):
-            return JsonResponse({'body': 'Request sent to lender', 'transactionNo': transaction.transactionNo }, status=200)
+        transactionID = transaction.transactionID
+        if sendPushNotification(lender.deviceID, "New Address Request", "New address request has been initiated", {'encryptedMessage': request.data['message'], 'transactionID': transaction.transactionID, 'status': transaction.state, 'requesterSC': requester.shareableCode}):
+            return JsonResponse({'body': 'Request sent to lender', 'transactionID': transactionID }, status=200)
         else:
             transaction.state = 'aborted'
             transaction.save()
-            return JsonResponse({'body': 'Unable to send request to lender. Aborting'}, status=400)
+            return JsonResponse({'body': 'Unable to send request to lender. Aborting', 'transactionID': transactionID}, status=400)
 
     return JsonResponse({}, status=400)
 
 
 @api_view(['POST'])
 @check_token
-@request_interface(['transactionNo'])
+@request_interface(['transactionID'])
 def rejectRequest(request):
     if request.method == 'POST':
         try:
-            transaction = Transaction.objects.get(transactionNo=request.data['transactionNo'])
+            transaction = Transaction.objects.get(transactionID=request.data['transactionID'])
+            assert(transaction.state == 'init')
         except:
-            return JsonResponse({'transactionNo': transaction.transactionNo}, status=400)
+            return JsonResponse({'body': "Invalid transactionID"}, status=400)
 
         requester = transaction.requester
         transaction.state = 'rejected'
         transaction.save()
 
-        sendPushNotification(requester.deviceID, "Address request denied", f"Address request denied for TNo: {transaction.transactionNo}", {'transactionNo': transaction.transactionNo})
-        return JsonResponse({'body': 'Request denied successfully', 'transactionNo': transaction.transactionNo }, status=200)
+        sendPushNotification(requester.deviceID, "Address request denied", f"Address request denied for TNo: {transaction.transactionID}", {'transactionID': transaction.transactionID})
+        return JsonResponse({'body': 'Request denied successfully', 'transactionID': transaction.transactionID }, status=200)
         
 
     return JsonResponse({}, status=400)
@@ -168,53 +210,63 @@ def getPublicKey(request):
 
 @api_view(['POST'])
 @check_token
-@request_interface(['txnID', 'eKYC', 'passcode', 'filename'])
+@request_interface(['transactionID', 'eKYC', 'passcode', 'filename'])
 def POSTekyc(request):
     if request.method == 'POST':
+        
+        transactionID = request.data['transactionID']
+        eKYC_enc = request.data['eKYC']
+        passcode_enc = request.data['passcode']
+        filename = request.data['filename']
+
         try:
-            transactionId = request.data['txnID']
-            eKYC_enc = request.data['eKYC']
-            passcode_enc = request.data['passcode']
-            filename = request.data['filename']
-
-            renterDeviceId = Transaction.objects.get(id=transactionId).requester.deviceID
-
+            transaction = Transaction.objects.get(id=transactionID)
+            renterDeviceId = transaction.requester.deviceID
+            assert(transaction.state == 'init')
+            transaction.state = 'accepted'
+            transaction.save()
             OfflineEKYC.objects.create(
-                transactionId=transactionId,
+                transactionID=transactionID,
                 encryptedEKYC=eKYC_enc,
                 encryptedPasscode=passcode_enc,
                 filename=filename,
             )
-            print(transactionId, eKYC_enc, passcode_enc, filename)
 
-            message_caption = "Address Request Approved!"
-            message_body = "Hi There! Landlord has approved your request to share his address, please click the button to get the address"
-            message_data = {
-                'transactionId': transactionId,
-                'status': "200"
-            }
-            if sendPushNotification(renterDeviceId, message_caption, message_body, message_data):
-                return JsonResponse({
-                    'message':'Hello from the server!', 
-                    'txnID': transactionId
-                    }, status=200)
-            return JsonResponse({'message': 'Push Notification Failure', 'txnID': '-1'}, status=500)
         except:
-            return JsonResponse({'message': 'POST request failed, please request again', 'txnID': '-1'}, status=500)
+            return JsonResponse({'message': 'POST request failed, please request again', 'transactionID': transactionID}, status=500)
 
-    return JsonResponse({'message': 'Please "POST" the request', 'txnID': '-1'}, status=400)
+        
+        # print(transactionID, eKYC_enc, passcode_enc, filename)
+
+        message_caption = "Address Request Approved!"
+        message_body = "Hi There! Landlord has approved your request to share his address, please click the button to get the address"
+        message_data = {
+            'transactionID': transactionID,
+            'status': "200"
+        }
+        if sendPushNotification(renterDeviceId, message_caption, message_body, message_data):
+            return JsonResponse({
+                'message':'Hello from the server!', 
+                'transactionID': transactionID,
+                'status': transaction.state
+                }, status=200)
+        return JsonResponse({'message': 'Push Notification Failure', 'transactionID': transactionID, 'status': transaction.state}, status=500)
+
+    return JsonResponse({'message': 'Please "POST" the request', 'transactionID': '-1'}, status=400)
 
 
 @api_view(['GET'])
 @check_token
-@request_interface(['txnID'])
+@request_interface(['transactionID'])
 def GETekyc(request):
     if request.method == 'GET':
+
+        transactionID = request.data['transactionID']
         try:
-            transactionId = request.data['txnID']
-            offlineEKYC = OfflineEKYC.objects.get(transactionId=transactionId)
+            transaction = Transaction.objects.get(id=transactionID)
+            assert(transaction.state == 'accepted')
+            offlineEKYC = OfflineEKYC.objects.get(transactionID=transactionID)
             
-            transaction = Transaction.objects.get(id=transactionId)
             transaction.status = 'shared'
             transaction.save()
             
@@ -222,9 +274,52 @@ def GETekyc(request):
                 'encryptedEKYC': offlineEKYC.encryptedEKYC,
                 'encryptedPasscode': offlineEKYC.encryptedPasscode,
                 'filename': offlineEKYC.filename,
-                'txnID': offlineEKYC.transactionId
+                'transactionID': offlineEKYC.transactionID,
+                'status': transaction.state
                 }, status=200)
         except:
-            return JsonResponse({'message': 'GET request failed, please request again', 'txnID': '-1'}, status=500)
+            return JsonResponse({'message': 'GET request failed, please request again', 'transactionID': transactionID}, status=500)
 
-    return JsonResponse({'message': 'Please "GET" the request!', 'txnID': '-1'}, status=400)
+    return JsonResponse({'message': 'Please "GET" the request!', 'transactionID': '-1'}, status=400)
+
+@api_view(['POST'])
+@check_token
+@request_interface(['transactionID', 'oldAddress', 'newAddress', 'gpsCoord', 'uid'])
+def updateAddress(request):
+    if request.method == 'POST':
+        transactionID = request.data['transactionID']
+        try:
+            requester = AnonProfile.objects.get(uidToken=request.data['uidToken'])
+            transaction = Transaction.objects.get(transactionID=transactionID)
+            assert(transaction.requester == requester)
+            assert(transaction.state == 'shared')
+        except:
+            return JsonResponse({'body': "Invalid transactionID"}, status=400)
+
+        oldCoord = getCoord(request.data['oldAddress'])
+        if not oldCoord:
+            return JsonResponse({'body': 'Old address invalid', 'transactionID': transactionID, 'status': transaction.state}, status=400)
+        
+        newCoord = getCoord(request.data['newAddress'])
+        if not newCoord:
+            return JsonResponse({'body': 'New address invalid', 'transactionID': transactionID, 'status': transaction.state}, status=400)
+        
+        gpsCoord = request.data['gpsCoord']
+        distance1 = getDistance(oldCoord, newCoord)
+        distance2 = getDistance(gpsCoord, newCoord)
+        # print(gpsCoord, distance1, distance2)
+        if distance1 < 0.4 and distance2 < 1:
+            try:
+                UpdatedAddress.objects.create(**(request.data['newAddress']), uid=request.data['uid'], transactionID=transactionID)
+                transaction.state = 'commited'
+                transaction.save()
+                sendPushNotification(transaction.lender.deviceID, "Requester's address has been updated", f"Requester's address for TNo: {transactionID} has been updated", {'requesterSC': requester.shareableCode, 'newAddress': request.data['newAddress']})
+                return JsonResponse({'transactionID': transactionID, 'status': transaction.state}, status=200)
+            except:
+                return JsonResponse({'body': 'Repeated TransactionID', 'transactionID': transactionID, 'status': transaction.state}, status=400)
+        return JsonResponse({'body': f'Addresses are too far. Dist from gps: {distance2}, dist from oldAddress: {distance1}', 'transactionID': transactionID, 'status': transaction.state}, status=400)
+
+    return JsonResponse({}, status=400)
+
+
+
