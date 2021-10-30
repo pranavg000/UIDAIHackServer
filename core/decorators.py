e38e3687d4319a7cb5fffc5c4922f4da916fc2ab
Pranav <pranavgupta000@gmail.com>
2021-10-30 15:07:24 +0530

    added logging in decorators

--- core/decorators.py
+++ core/decorators.py
@@ -3,14 +3,17 @@ from json.encoder import JSONEncoder
 from django.http import HttpResponseRedirect
 from django.http.response import JsonResponse
 from .models import AnonProfile
+import logging
 
+genLogger = logging.getLogger('django.server')
 
 def check_token(func):
     @request_interface(['uidToken', 'authToken'])
     def wrap(request, *args, **kwargs):
         if AnonProfile.objects.filter(uidToken=request.data['uidToken'], authToken=request.data['authToken']).exists():
             return func(request, *args, **kwargs)
-        return JsonResponse({}, status=401)
+        genLogger.warning(f"{request.data['uidToken']}:Invalid auth token")
+        return JsonResponse({"message": "Invalid authToken"}, status=401)
     wrap.__doc__ = func.__doc__
     wrap.__name__ = func.__name__
     return wrap
@@ -20,7 +23,8 @@ def request_interface(keyList):
         def wrap(request, *args, **kwargs):
             for key in keyList:
                 if key not in request.data:
-                    return JsonResponse({"KeyNotFound": key}, status=400)
+                    genLogger.warning(f"KeyNotFound: {key}")
+                    return JsonResponse({"message": f"KeyNotFound: {key}"}, status=400)
             return func(request, *args, **kwargs)
         return wrap
     return decorator
